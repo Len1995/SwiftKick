fb845205398a25bc34abe2c0de84a25e71d3c011
20063251 <20063251@it102imac15.wit.ie>
2015-11-13 15:10:39 +0000

    Added four sound buttons, with sound working on click

--- SwiftKick/ViewController.swift
+++ SwiftKick/ViewController.swift
@@ -12,6 +12,10 @@ import AudioToolbox
 class ViewController: UIViewController {
 
     var kickSound: SystemSoundID!
+    var snareSound: SystemSoundID!
+    var clapSound: SystemSoundID!
+    var hihatSound: SystemSoundID!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -29,11 +33,39 @@ class ViewController: UIViewController {
         var soundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "kick", "wav", nil)
         AudioServicesCreateSystemSoundID(soundURL, &soundID)
         kickSound = soundID
-    }
+        soundID++
+        
+        soundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "snare", "wav", nil)
+        AudioServicesCreateSystemSoundID(soundURL, &soundID)
+        snareSound = soundID
+        soundID++
+        
+        soundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "clap", "wav", nil)
+        AudioServicesCreateSystemSoundID(soundURL, &soundID)
+        clapSound = soundID
+        soundID++
+        
+        soundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "hihat", "wav", nil)
+        AudioServicesCreateSystemSoundID(soundURL, &soundID)
+        hihatSound = soundID     }
     
     @IBAction func playKick(sender: AnyObject) {
         print("Playing Kick Sound")
         AudioServicesPlaySystemSound(kickSound)
+    }
+    
+    @IBAction func playSnare(sender: AnyObject) {
+        print("Playing Snare Sound")
+        AudioServicesPlaySystemSound(snareSound)
+    }
+    @IBAction func playClap(sender: AnyObject) {
+        print("Playing Clap Sound")
+        AudioServicesPlaySystemSound(clapSound)
+    }
+    
+    @IBAction func playHihat(sender: AnyObject) {
+        print("Playing Hihat Sound")
+        AudioServicesPlaySystemSound(hihatSound)
     }
 
 }
